33d76187f20d75c1852f13246ac3700a8e4b3813
AndersM123 <69676974+AndersM123@users.noreply.github.com>
2020-09-10 18:55:30 +0200

    Oppgaver fra Java boken.

---
+++ Oppgaver/src/Oppgaver_kapittel_2.java
@@ -0,0 +1,147 @@
+import static javax.swing.JOptionPane.*;
+import static java.lang.Math.*;
+
+public class Oppgaver_kapittel_2 {
+
+	
+	/*
+//Oppgave 1
+	public static void main(String[] args) {
+		String tall_1 = showInputDialog("Skriv det første tallet: ");
+		String tall_2 = showInputDialog("Skriv tall 2: ");
+		
+		int a = Integer.parseInt(tall_1);
+		int b = Integer.parseInt(tall_2);
+		
+		int sum = a+b;
+		
+		showMessageDialog(null, "Summen av de to tallene er: " + sum);
+		
+	}
+	*/
+	
+	
+/*	
+//Oppgave 2
+	public static void main(String[] args) {
+		
+		String tall_bredde = showInputDialog("Skriv bredden: ");
+		String tall_høyde = showInputDialog("Skriv lengden: ");
+		
+		double bredde = Double.parseDouble(tall_bredde);
+		double høyde = Double.parseDouble(tall_høyde);
+		
+		double areal = bredde*høyde;
+		
+		showMessageDialog(null, "Arealet er: " + areal);
+		
+		
+
+	}
+
+	*/
+
+		
+	/*
+//Oppgave 3
+	public static void main(String[] args) {
+// Formelen for arealet av en sirkel er = PI*r^2.
+
+		String radius = showInputDialog("Skriv inn radius: ");
+		
+		double r = Double.parseDouble(radius);
+		
+		double arealavsirkel = Math.PI * r * r;
+		
+		showMessageDialog(null, "Arealet av sirkelen er: " + arealavsirkel);
+
+	
+		
+	}
+	
+	*/
+	/*
+//Oppgave 4
+	public static void main(String[] args) {
+		String dollar = showInputDialog("Skriv inn beløpet i dollar");
+		
+		double d = Double.parseDouble(dollar);
+		
+		double DollartoNOK = d*8.9316;
+		
+		showMessageDialog(null, "Summen dollar i norske kroner er: " + DollartoNOK);
+		
+	}
+*/
+	
+/*
+//Oppgave 5
+	public static void main(String[] args) {
+		String tall = showInputDialog("Skriv inn et tall mellom 0 og 1000: ");
+		
+		int heltall = Integer.parseInt(tall);
+		
+		int d_1 = heltall/100;
+		int r_1 = heltall%100;
+		
+		int r_1_2 = r_1/10;
+		int r_2 = r_1%10;
+		
+		int sum = d_1 + r_1_2 + r_2;
+		
+		showMessageDialog(null, "Summen av sifrene til innslått tall er: " + sum);
+		
+		
+	}
+		*/
+	/*
+//Oppgave 6
+	public static void main(String[] args) {
+		String tall_1 = showInputDialog("Skriv inn tall nummer 1: ");
+		String tall_2 = showInputDialog("Skriv inn tall nummer 2: ");
+		String tall_3 = showInputDialog("Skriv inn tall nummer 3: ");
+		String tall_4 = showInputDialog("Skriv inn tall nummer 4: ");
+		
+		int a = Integer.parseInt(tall_1);
+		int b = Integer.parseInt(tall_2);
+		int c = Integer.parseInt(tall_3);
+		int d = Integer.parseInt(tall_4);
+		
+		int tall12 = min(a, b);
+		int tall34 = min(c, d);
+		int tall1234 = min(tall12, tall34);
+		
+		showMessageDialog(null, "Det minste tallet av de fire du skrev inn er: " + tall1234);
+		
+	}
+	*/
+	
+	/*
+//Oppgave fra PowerPoint om Moms
+	public static void main(String[] args) {
+		String beløp = showInputDialog("Skriv inn beløpet: ");
+		
+		double b = Integer.parseInt(beløp);
+		
+		double moms = b*0.25;
+		double totalpris = b*1.25;
+		
+		showMessageDialog(null, "Kvittering: " + "Moms: " + moms + " Totalpris: " + totalpris);
+		
+		
+		
+		
+	}
+	
+	
+	
+	*/
+	
+}
+	
+	
+	
+	
+	
+	
+
