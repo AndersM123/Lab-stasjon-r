e8bfab12866efd7ce37ec6901a1977940263fb1a
AndersM123 <69676974+AndersM123@users.noreply.github.com>
2020-09-10 18:56:33 +0200

    Oppgaver fra Java boken.

---
+++ Oppgaver/src/Oppgaver_kapittel_3.java
@@ -0,0 +1,42 @@
+
+import static javax.swing.JOptionPane.*;
+import static java.lang.ArithmeticException.*;
+
+public class Oppgaver_kapittel_3 {
+
+
+//Oppgave 1
+	/*
+	public static void main(String[] args) {
+		String tall_a = showInputDialog("Skriv inn tall nummer 1 her: ");
+		String tall_b = showInputDialog("Skriv inn tall nummer 2 her: ");
+		
+		int a = Integer.parseInt(tall_a);
+		int b = Integer.parseInt(tall_b);
+		
+		if (b > 0) {
+			showMessageDialog(null, "Resulatet av divisjonen a/b= " + a/b);
+			
+		}
+		else  {
+			showMessageDialog(null, "Error, tallet du oppga er ikke større enn 0!");
+			
+		}
+		
+
+	}
+	*/
+	
+	
+	
+	
+	
+	
+			
+		
+		
+		
+	
+	
+
+}
